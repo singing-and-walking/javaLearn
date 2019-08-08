d1ea93ab1b6bec14cb662e07c7793bbe91362d69
雨 <@192.168.0.101>
2019-08-08 14:19:39 +0800

    为User添加成员变量age

--- Java学习示例代码/src/been/User.java
+++ Java学习示例代码/src/been/User.java
@@ -9,6 +9,17 @@ public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	private String name;
 	private String password;
+	private int age;
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 	public String getName() {
 		return name;
 	}
