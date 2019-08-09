60c67ef0b1f88382738f6d0eee890cb89bf10627
雨 <@192.168.0.101>
2019-08-09 11:30:39 +0800

    同步User里的一些方法

--- Java学习示例代码/src/been/User.java
+++ Java学习示例代码/src/been/User.java
@@ -2,7 +2,7 @@ package been;
 
 import java.io.Serializable;
 
-public class User implements Serializable{
+public class User implements Serializable {
 	/**
 	 * 
 	 */
@@ -10,27 +10,37 @@ public class User implements Serializable{
 	private String name;
 	private String password;
 	private int age;
-	
-	public int getAge() {
+
+	public synchronized int getAge() {
 		return age;
 	}
-	public void setAge(int age) {
+
+	public synchronized void setAge(int age) {
 		this.age = age;
 	}
+
+	public synchronized void addAge() {
+		age++;
+	}
+
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 }
