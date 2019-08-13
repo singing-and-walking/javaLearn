e2d9c60091f11800a463b696eb654f366b52b9d1
雨 <@192.168.0.102>
2019-08-13 19:34:42 +0800

    服务器ip改为localhost

--- Java学习示例代码/src/socketBase/Client.java
+++ Java学习示例代码/src/socketBase/Client.java
@@ -6,7 +6,7 @@ import java.net.UnknownHostException;
 
 public class Client {
 	public static void main(String[] args) throws UnknownHostException, IOException {
-		Socket s=new Socket("192.168.0.101",8086);
+		Socket s=new Socket("localhost",8086);
 		s.getOutputStream().write('a');
 	}
 }
