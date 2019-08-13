b059ef503fc665024b3337e29890425011ac70b6
雨 <@192.168.0.102>
2019-08-13 19:37:03 +0800

    随笔

---
+++ Java学习示例代码/src/Test.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+	public static void main(String[] args) {
+		List<Number> list =new ArrayList<>();
+		list.add(0);
+		System.out.println(list.get(0));
+	}
+}
